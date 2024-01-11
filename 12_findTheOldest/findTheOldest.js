8cd154d241d5a578916d5ab1dcc3a964cacff1d8
Rachad-4 <rachadneyland@gmail.com>
2024-01-10 18:58:55 -0600

    Add logic for findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(person) {
+    let oldest = 0; 
+    let age = 0;
+    let date = new Date();
+    let currentYear = date.getFullYear();
 
+    for (let i = 0; i < person.length; i++) {
+        if (age < ((person[i].yearOfDeath || currentYear) - person[i].yearOfBirth)){
+            age = (person[i].yearOfDeath || currentYear) - person[i].yearOfBirth;
+            oldest = i;
+        }
+    }
+    return person[oldest];
 };
 
 // Do not edit below this line
